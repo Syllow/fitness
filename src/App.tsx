6c90609a20ec0184d63cf62ced28844f1000bd1b
Vincent <vince.derrien@gmail.com>
2024-06-28 15:21:53 +0000

    ✨ Création de la section "Home"

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,34 @@
 import Navbar from '@/scenes/navbar';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { SelectedPage } from '@/shared/types';
+import Home from './scenes/home';
 
 function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(
     SelectedPage.Home
   );
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage(SelectedPage.Home);
+      }
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    };
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
     <div className='app bg-gray-20'>
-      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+      <Navbar
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
+      <Home setSelectedPage={setSelectedPage} />
     </div>
   );
 }
@@ -16,4 +36,4 @@ function App() {
 export default App;
 
 // https://www.youtube.com/watch?v=I2NNxr3WPDo
-// 1:30:00
+// 1:52:00
