a3e7c748d993df25201ee98b2e4c8e51f38eb8bc
Vincent <vince.derrien@gmail.com>
2024-07-01 12:25:51 +0000

    Fini !

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@ import Navbar from '@/scenes/navbar';
 import { useEffect, useState } from 'react';
 import { SelectedPage } from '@/shared/types';
 import Home from './scenes/home';
+import Benefits from './scenes/benefits';
+import OurClasses from './scenes/ourClasses';
+import ContactUs from './scenes/contactUs';
+import Footer from './scenes/footer';
 
 function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(
@@ -29,6 +33,10 @@ function App() {
         setSelectedPage={setSelectedPage}
       />
       <Home setSelectedPage={setSelectedPage} />
+      <Benefits setSelectedPage={setSelectedPage} />
+      <OurClasses setSelectedPage={setSelectedPage} />
+      <ContactUs setSelectedPage={setSelectedPage} />
+      <Footer />
     </div>
   );
 }
@@ -37,3 +45,6 @@ export default App;
 
 // https://www.youtube.com/watch?v=I2NNxr3WPDo
 // 1:52:00
+
+// A voir :
+// https://www.framer.com/motion/
--- src/shared/types.ts
+++ src/shared/types.ts
@@ -8,3 +8,15 @@ export enum SelectedPage {
 export type SetSelectedPage = React.Dispatch<
   React.SetStateAction<SelectedPage>
 >;
+
+export interface BenefitType {
+  icon: JSX.Element;
+  title: string;
+  description: string;
+}
+
+export interface ClassType {
+  name: string;
+  description?: string;
+  image: string;
+}
